848f9ec4d983a5cedf3817b25eb138c5b272b549
Programmer-JK31 <jaydeepkotadiya31@gmail.com>
2025-03-17 11:35:03 +0530

    Added Url in cors

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 
 connectToMongo();
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors("i-notebook-lyart.vercel.app"));
 app.use(express.json());
 
-app.use('/api/auth' , require('./routes/auth.js'));
-app.use('/api/notes' , require('./routes/notes.js'));
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`iNotebook backend is listening at http://localhost:${port}`);
 });
